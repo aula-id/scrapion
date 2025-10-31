cd66ce11f3ae5ca775cb6cc4aaaaad8dd4e8fa4b
zonblade <agungzonblade@gmail.com>
2025-10-31 19:22:57 +0900

    update repos data

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="scrapion",
-    version="0.1.0",
+    version="0.1.1",
     author="Aula Dev",
     author_email="contact@aula.id",
     description="Web scraping automation system with intelligent fallback mechanisms",
